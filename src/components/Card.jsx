d8d9a64690261a402e76e76db2be0563a8d6d07d
Özgür Aktaş <ozguraktas.55555@gmail.com>
2023-07-20 19:48:25 +0300

    20.07.2023

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./scss/style.scss";
 import { FaHouseChimney } from "react-icons/fa6";
 import { FaCloud } from "react-icons/fa6";
 import { FaShareNodes } from "react-icons/fa6";
 import { FaMagnifyingGlass } from "react-icons/fa6";
-
 import { Container, Button, Form, InputGroup } from "react-bootstrap";
+import axios from "axios";
+
+const API_KEY = import.meta.env.WEATHER_API_KEY;
 
 const Card = () => {
+  const [currentDate, setCurrentDate] = useState("");
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState({});
+
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const apiKey = API_KEY;
+
+    axios
+      .get(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+      .then((response) => {
+        setWeatherData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error", error);
+      });
+  };
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      const date = new Date();
+      setCurrentDate(formatDate(date));
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
+  const formatDate = (date) => {
+    const options = {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    };
+    return date.toLocaleDateString(undefined, options);
+  };
   return (
     <Container className="cards-all">
       <div className="cards-left">
         <div className="button-up">
-          <div className="home">
+          <Button className="home">
             <FaHouseChimney />
             <p>Home</p>
-          </div>
-          <div className="forecast">
+          </Button>
+          <Button className="forecast">
             <FaCloud />
             <p>Forecast</p>
-          </div>
-          <div className="share">
+          </Button>
+          <Button className="share">
             <FaShareNodes />
             <p>Share</p>
-          </div>
+          </Button>
         </div>
         <div className="card-bottom">
           <div className="icon">
-            <i class="wi wi-day-haze"></i>
+            <i className="wi wi-day-haze"></i>
           </div>
 
           <p className="icon-text">MOSTLY SUNNY</p>
@@ -35,18 +76,29 @@ const Card = () => {
 
           <div className="search">
             <InputGroup className="mb-3">
-              <Button variant="success" id="button-addon1">
+              <Button
+                className="input-icon"
+                variant="success"
+                id="button-addon1"
+                onClick={handleSubmit}
+              >
                 <FaMagnifyingGlass />
               </Button>
               <Form.Control
+                type="text"
+                value={city}
+                onChange={handleChange}
                 aria-label="Example text with button addon"
                 aria-describedby="basic-addon1"
+                placeholder="Enter city name..."
               />
             </InputGroup>
           </div>
 
-          <p>SAT, 2 JULY 2015</p>
-          <p>Copanhagen</p>
+          <p className="date">{currentDate}</p>
+          {weatherData.location && weatherData.location.name && (
+            <p className="city">{weatherData.location.name}</p>
+          )}
         </div>
       </div>
       <div className="cards-right">
@@ -60,47 +112,47 @@ const Card = () => {
           <div className="first-three-day">
             <div>
               <div className="icon">
-                <i class="wi wi-rain"></i>
+                <i className="wi wi-rain"></i>
               </div>
-              <p>24°C</p>
-              <p>Sun. 3 July</p>
+              <p className="degrees">24°C</p>
+              <p className="date">Sun. 3 July</p>
             </div>
             <div>
               <div className="icon">
-                <i class="wi wi-rain-mix"></i>
+                <i className="wi wi-rain-mix"></i>
               </div>
-              <p>32°C</p>
-              <p>Mon. 4 July</p>
+              <p className="degrees">32°C</p>
+              <p className="date">Mon. 4 July</p>
             </div>
             <div>
               <div className="icon">
-                <i class="wi wi-strong-wind"></i>
+                <i className="wi wi-strong-wind"></i>
               </div>
-              <p>28°C</p>
-              <p>Sat. 5 July</p>
+              <p className="degrees">28°C</p>
+              <p className="date">Sat. 5 July</p>
             </div>
           </div>
           <div className="second-three-day">
             <div>
               <div className="icon">
-                <i class="wi wi-night-alt-cloudy"></i>
+                <i className="wi wi-night-alt-cloudy"></i>
               </div>
-              <p>21°C</p>
-              <p>Wed. 6 July</p>
+              <p className="degrees">21°C</p>
+              <p className="date">Wed. 6 July</p>
             </div>
             <div>
               <div className="icon">
-                <i class="wi wi-day-sunny"></i>
+                <i className="wi wi-day-sunny"></i>
               </div>
-              <p>25°C</p>
-              <p>Thu. 7 July</p>
+              <p className="degrees">25°C</p>
+              <p className="date">Thu. 7 July</p>
             </div>
             <div>
               <div className="icon">
-                <i class="wi wi-rain"></i>
+                <i className="wi wi-rain"></i>
               </div>
-              <p>18°C</p>
-              <p>Fri. 8 July</p>
+              <p className="degrees">18°C</p>
+              <p className="date">Fri. 8 July</p>
             </div>
           </div>
         </div>
